74e09c6be6b1101d2ba4b514f97e9ecbd906f8e8
Gunnar Gabrielson <gunnargabrielson@gmail.com>
2017-09-24 18:36:38 -0600

    circle ci init

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <h3>TESTING!</h3>
         </p>
       </div>
     );
